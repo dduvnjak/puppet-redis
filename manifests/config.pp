16a6a3384abf992dfa79c1f1b533f2cc09dbd4f6
Dario Duvnjak <dario.duvnjak@gmail.com>
2013-08-12 14:34:46 +0200

    Fix missing path

--- manifests/config.pp
+++ manifests/config.pp
@@ -5,6 +5,7 @@ class redis::config {
   exec { 'redis_cleanup':
     command => 'stop redis-server; rm -rf /etc/init/redis-server.conf /etc/redis/redis.conf /var/log/redis/redis.log /var/lib/redis',
     onlyif  => 'test -f /etc/init/redis-server.conf',
+    path    => [ "/sbin/", "/usr/bin" ],
   }
 
 }
